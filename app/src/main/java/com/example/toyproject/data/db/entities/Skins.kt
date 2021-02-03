3ca00cb8664da5a9b9e721c1649883d72bf82cb8
SeungYooon <tmddbs4998@naver.com>
2021-02-03 22:22:19 +0900

    Clean Up Codes

---
+++ app/src/main/java/com/example/toyproject/data/db/entities/Skins.kt
@@ -0,0 +1,30 @@
+package com.example.toyproject.data.db.entities
+
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+@Entity(tableName = "skins")
+@Parcelize
+data class Skins(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int? = null,
+    @ColumnInfo(name = "skin_title")
+    @SerializedName("skin_title")
+    val skinTitle: String = "",
+    @ColumnInfo(name = "skin_kinds")
+    @SerializedName("skin_kinds")
+    val skinKinds: String = "",
+    @ColumnInfo(name = "image_url")
+    @SerializedName("image_url")
+    val imageUrl: String = "",
+    @ColumnInfo(name = "description")
+    @SerializedName("description")
+    val description: String = "",
+    @ColumnInfo(name = "point")
+    @SerializedName("point")
+    val point: String = ""
+) : Parcelable
