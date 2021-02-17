3d3fccd9a0cbc2854566454765059f8f9b4a71d3
SeungYooon <tmddbs4998@naver.com>
2021-02-17 20:39:45 +0900

    Clean Up Code

--- app/src/main/java/com/example/toyproject/util/extensions/SkinInfoDiffCallback.kt
+++ app/src/main/java/com/example/toyproject/util/extensions/SkinInfoDiffCallback.kt
@@ -5,7 +5,7 @@ import com.example.toyproject.data.db.entities.SkinInfo
 
 class SkinInfoDiffCallback : DiffUtil.ItemCallback<SkinInfo>() {
     override fun areItemsTheSame(oldItem: SkinInfo, newItem: SkinInfo): Boolean =
-        oldItem.skinKinds == newItem.skinKinds
+        oldItem.id == newItem.id
 
     override fun areContentsTheSame(oldItem: SkinInfo, newItem: SkinInfo): Boolean =
         oldItem == newItem
